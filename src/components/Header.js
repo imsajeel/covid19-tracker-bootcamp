d09c2425e1a435df2b629d40f13197324e7556f4
Sajeel Aalam <imsajeel.dev@gmail.com>
2020-07-04 14:45:41 +0500

    Some css transitions added

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Header() {
   return (
     <header>
-      <b>Covid 19 Tracker</b> by Sajeel Aalam
+      <b>COVID-19 Tracker</b> by Sajeel Aalam
     </header>
   );
 }
